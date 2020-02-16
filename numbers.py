cea299014934504d9eea842fb104e816c90adada
MikeTheFyke <mikefyke@hotmail.com>
2020-02-16 16:34:35 -0500

    python math

--- numbers.py
+++ numbers.py
@@ -4,3 +4,18 @@ print (float(number))
 # equals 2.5
 print (int(number))
 # equals 2
+
+number2 = 14
+
+print (number + number2)
+
+print (number - number2)
+
+print (number * number2)
+
+print (number / number2)
+
+print (number // number2)
+# Integer division - Round down so there are no decimal places
+print (number ** number2)
+# Exponentiation (number to the power of number2)
