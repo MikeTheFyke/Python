9d2994347bf4110acc7e3aab1d911600a78b7387
MikeTheFyke <mikefyke@hotmail.com>
2020-02-16 16:11:22 -0500

    Journey text

---
+++ strings.py
@@ -0,0 +1,9 @@
+journey = """Just a small tone girl 
+Leaving in a lonely whirl
+She took the midnight tray going anywhere
+Just a seedy boy
+Bored and raised in South Detroit or
+something
+He took the midnight tray going anywhere"""
+
+print(journey)
