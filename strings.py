1bbefe52ed8f70fe88b29012d3871b1db5922479
MikeTheFyke <mikefyke@hotmail.com>
2020-02-16 16:23:29 -0500

    replace method executed

--- strings.py
+++ strings.py
@@ -1,9 +1,14 @@
-journey = """Just a small tone girl 
+journey = """Just a small tone girl
 Leaving in a lonely whirl
 She took the midnight tray going anywhere
 Just a seedy boy
 Bored and raised in South Detroit or
 something
 He took the midnight tray going anywhere"""
+
+journey = journey.replace("tone", "town")
+journey = journey.replace("whirl", "world")
+journey = journey.replace("tray", "train")
+journey = journey.replace("seedy", "city")
 
 print(journey)
