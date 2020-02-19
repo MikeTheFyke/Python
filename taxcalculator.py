ee90eec53808c551436442da990b9b613ed66492
MikeTheFyke <mikefyke@hotmail.com>
2020-02-19 13:22:44 -0500

    Added comments to taxcalculator logic

--- taxcalculator.py
+++ taxcalculator.py
@@ -4,7 +4,7 @@ print("")
 subtotal = input("Please enter your subtotal : $")
 tax = subtotal * .13
 total = subtotal + tax
-
+# '${:0,.2f}'.format(_____) converts and displays in a currency friendly format
 print("Your Total tax equals      = " +  '${:0,.2f}'.format(tax))
 print("Your Total with tax equals = " +  '${:0,.2f}'.format(total))
 print("")
