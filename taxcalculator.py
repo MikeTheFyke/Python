f94f637d23e88bbeac2550aa76aeaccb84b52279
MikeTheFyke <mikefyke@hotmail.com>
2020-02-19 12:49:45 -0500

    Added currentcy and rounding format function

--- taxcalculator.py
+++ taxcalculator.py
@@ -3,6 +3,7 @@ tax = subtotal * .13
 total = subtotal + tax
 
 
-
 print("Your Total tax equals = " +  str(tax))
 print("Your Total with tax equals = " +  str(total))
+
+print ('${:0,.2f}'.format(total))
