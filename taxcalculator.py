87a2b6ba9ff39c65536d9198534bccdc90ef3279
MikeTheFyke <mikefyke@hotmail.com>
2020-02-19 12:33:24 -0500

    Added Dummy values

---
+++ taxcalculator.py
@@ -0,0 +1,8 @@
+subtotal = 10
+tax = subtotal * .13
+total = subtotal + tax
+
+
+print("Please enter your subtotal : "+  str(subtotal))
+print("Your Total tax equals = " +  str(tax))
+print("Your Total with tax equals = " +  str(total))
