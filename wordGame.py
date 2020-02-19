d6dc9167f00f15f3f9820ecc9aee82a3c8cac72f
MikeTheFyke <mikefyke@hotmail.com>
2020-02-19 13:52:56 -0500

    Added multi lines

--- wordGame.py
+++ wordGame.py
@@ -1,6 +1,5 @@
 placeInput = raw_input(" Please Enter the name of a Place   : ")
 objectInput = raw_input(" Please Enter the name of an object : ")
 
-
-print ("Just as I arrived at " + str(placeInput))
-print (", I realized I had forgotten my " + str(objectInput))
+print ("Alrighty here's your result ..." + "\n")
+print ("" + "\n      Just as I arrived at " + str(placeInput) + "\n, I realized I had forgotten my " + str(objectInput) + " in the bathroom." + "\n")
