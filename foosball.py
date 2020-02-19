b652aa1426fb19d5db8b9d00cce40096b13e4921
MikeTheFyke <mikefyke@hotmail.com>
2020-02-19 16:21:50 -0500

    Added Average Player to The Evils

--- foosball.py
+++ foosball.py
@@ -22,10 +22,14 @@ Derangers = [
 
 # Find the middle player
 middlePlayer = ((len(screamingEvils)) / 2)
-print("The Middle Man Of The Screaming Evils " + str(screamingEvils[middlePlayer - 1]))
+print("\nThe Middle Man Of The Screaming Evils " + str(screamingEvils[middlePlayer - 1]))
 
 # Display 2 above and 2 below the middle player
 # Above
 print("2 Above : " + str(screamingEvils[middlePlayer - 3: middlePlayer - 1]))
 # Below 
 print("2 Below" + str(screamingEvils[middlePlayer: middlePlayer + 2]))
+
+# Inserting An Average Player To Represent the middle player on the team
+screamingEvils.insert(middlePlayer,"Average Johnson")
+print("Your New Screaming Evils \n" + str(screamingEvils) + "\n")
