ac7e4eeff6d412f95f58d2b071249542ac38f665
MikeTheFyke <mikefyke@hotmail.com>
2020-02-19 14:01:34 -0500

    format multi line spacing by conlidating their placement

--- wordGame.py
+++ wordGame.py
@@ -1,5 +1,8 @@
-placeInput = raw_input(" Please Enter the name of a Place   : ")
-objectInput = raw_input(" Please Enter the name of an object : ")
+print("\nWelcome To Your Virtual Python AbLib WordGame\n")
 
-print ("Alrighty here's your result ..." + "\n")
-print ("" + "\n      Just as I arrived at " + str(placeInput) + "\n, I realized I had forgotten my " + str(objectInput) + " in the bathroom." + "\n")
+placeInput = raw_input(" Please Enter The Name Of A Place   : ")
+objectInput = raw_input(" Please Enter The Name Of An Object : ")
+
+print ("\n     Alrighty here's your result ...\n .......................................")
+print ("\n      Just as I arrived at " + str(placeInput) + ",\n  I realized I had forgotten my " + str(objectInput) + "\n           in the bathroom.\n")
+print ("          Thanks for Playing.\n                   :)  \n")
