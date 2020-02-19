e8e17de34a342974c71ccdce1be8ae68ac292a05
MikeTheFyke <mikefyke@hotmail.com>
2020-02-19 15:19:22 -0500

    Added third access functionality

--- lists.py
+++ lists.py
@@ -8,9 +8,17 @@ people = [
 ]
 
 # First Access Method
+print("\n")
 print(people[0])
 print(people[2])
 print(people[4])
+print("\n")
+print("Last Position Is " + str(people[-1]))
 
 # Second Access Method
 print("Julian is #" + str(people.index("Julian")))
+print("\n")
+
+# Third Access Method
+print("The Top 3 are " + str(people[0:3]))
+print("\n")
