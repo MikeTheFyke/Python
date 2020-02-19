52f648aa64e82ef1bfb62d0c36114c1d4950338c
MikeTheFyke <mikefyke@hotmail.com>
2020-02-19 14:03:24 -0500

    Added comments

--- wordGame.py
+++ wordGame.py
@@ -1,8 +1,8 @@
 print("\nWelcome To Your Virtual Python AbLib WordGame\n")
-
+# raw_input() allowed for string input
 placeInput = raw_input(" Please Enter The Name Of A Place   : ")
 objectInput = raw_input(" Please Enter The Name Of An Object : ")
 
 print ("\n     Alrighty here's your result ...\n .......................................")
-print ("\n      Just as I arrived at " + str(placeInput) + ",\n  I realized I had forgotten my " + str(objectInput) + "\n           in the bathroom.\n")
+print ("\n      Just as I arrived at " + placeInput + ",\n  I realized I had forgotten my " + objectInput + "\n           in the bathroom.\n")
 print ("          Thanks for Playing.\n                   :)  \n")
