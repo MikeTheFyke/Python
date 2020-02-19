38bb480ea7847f83a4ef23d84dc90cae403f17cc
MikeTheFyke <mikefyke@hotmail.com>
2020-02-19 12:57:13 -0500

    Finalized layout format

--- taxcalculator.py
+++ taxcalculator.py
@@ -1,9 +1,12 @@
-subtotal = input("Please enter your subtotal : ")
+print("")
+print("_____Welcome To Python Tax Calculator_____")
+print("")
+subtotal = input("Please enter your subtotal : $")
 tax = subtotal * .13
 total = subtotal + tax
 
-
-print("Your Total tax equals = " +  str(tax))
-print("Your Total with tax equals = " +  str(total))
-
-print ('${:0,.2f}'.format(total))
+print("Your Total tax equals      = " +  '${:0,.2f}'.format(tax))
+print("Your Total with tax equals = " +  '${:0,.2f}'.format(total))
+print("")
+print("__________Okay Thank You__________")
+print("")
