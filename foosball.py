299601b824be602928437629a74f02ad2115a98c
MikeTheFyke <mikefyke@hotmail.com>
2020-02-19 16:02:40 -0500

    Foosball initial data added

---
+++ foosball.py
@@ -0,0 +1,21 @@
+screamingEvils = [
+    "Bones Jackson",
+    "Madman",
+    "Madboy",
+    "KT Slayer",
+    "Malicious Malone",
+    "Slick Toxin",
+    "Spewter",
+    "Mo"
+]
+
+Derangers = [
+    "Razor Kidd",
+    "Darkstar",
+    "Thrasher",
+    "Cannonball",
+    "Jackie LaGrunge",
+    "Joe Magician",
+    "Grim McSlam",
+    "Liquid Lazer"
+]
