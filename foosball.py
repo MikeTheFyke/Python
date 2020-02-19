f55121beb2041407b4cecccc1a898163364dd209
MikeTheFyke <mikefyke@hotmail.com>
2020-02-19 16:16:17 -0500

    Above and middle selection logic

--- foosball.py
+++ foosball.py
@@ -22,4 +22,10 @@ Derangers = [
 
 # Find the middle player
 middlePlayer = ((len(screamingEvils)) / 2)
-print(screamingEvils[middlePlayer - 1])
+print("The Middle Man Of The Screaming Evils " + str(screamingEvils[middlePlayer - 1]))
+
+# Display 2 above and 2 below the middle player
+# Above
+print("2 Above : " + str(screamingEvils[middlePlayer - 3: middlePlayer - 1]))
+# Below 
+print("2 Below" + str(screamingEvils[middlePlayer: middlePlayer + 2]))
