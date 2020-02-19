a61197d584fa6b839568c022782f7797bd5fd7ed
MikeTheFyke <mikefyke@hotmail.com>
2020-02-19 14:43:00 -0500

    Added replace functions

--- wordGame2.py
+++ wordGame2.py
@@ -22,6 +22,18 @@ NAME
 """
 
 ocInput = raw_input(" Please Enter An Occupation   : ")
+countryInput = raw_input(" Please Enter A Country   : ")
+pluralNounInput = raw_input(" Please Enter A Plural Noun   : ")
+verbInput = raw_input(" Please Enter An Action   : ")
+adjectiveInput = raw_input(" Please Enter An Adjective   : ")
+personalItem = raw_input(" Please Enter A Personal Item    : ")
 
-proseString.replace("OCCUPATION",ocInput)
-print(proseString)
+
+proseString2 = proseString.replace("OCCUPATION",ocInput)
+proseString2 = proseString.replace("COUNTRY",countryInput)
+proseString2 = proseString.replace("PLURAL_NOUN",pluralNounInput)
+proseString2 = proseString.replace("VERB",verbInput)
+proseString2 = proseString.replace("ADJECTIVE",adjectiveInput)
+proseString2 = proseString.replace("PERSONAL_ITEM",personalItem)
+
+print(proseString2)
