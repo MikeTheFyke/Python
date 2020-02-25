df6b5ad5c43fa1b678cf629620079160cb455391
MikeTheFyke <mikefyke@hotmail.com>
2020-02-25 12:43:25 -0500

    Initial commit data for archerloop

---
+++ archerLoop.py
@@ -0,0 +1,19 @@
+agents = [
+    "Malory Archer            01",
+    "Sterling Archer          02",
+    "Lawna Kane               03",
+    "Cyril Figgis             04",
+    "Pam Poovey               05",
+    "Cheryl Tunt              06",
+    "Dr. Krieger              07",
+    "Ray Gillette             08",
+    "Barry Dillon             09"
+]
+
+print ("\n--- Welcome To The Figgis Agency ---" + "\n---    List Of Active Agents     ---\n")
+
+
+for name in agents:
+    print name
+
+placeInput = raw_input("\n Please Enter An Agents extension Number   : ")
