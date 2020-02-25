2e1368a8ebb7ba4739e77189a2b789cb31599e72
MikeTheFyke <mikefyke@hotmail.com>
2020-02-25 12:04:55 -0500

    Added directions loop

--- loop.py
+++ loop.py
@@ -14,3 +14,24 @@ print("\n--This is the start of the list--")
 for name in rockosML:
     print name
 print("---This is the end of the list---\n")
+
+# Directions Loop
+
+print("\nLet's Get You Moving\n")
+
+directions = [
+    "turn left",
+    "go straight",
+    "turn right",
+    "keep going until you see the dog statue",
+    "turn right",
+    "turn right again",
+    "park right on the sidewalk"
+]
+
+instructions = "First "
+
+for nextDirection in directions:
+    instructions = instructions + nextDirection + ", then \n"
+
+print(instructions + "\nYou Have Arrived :)\n")
