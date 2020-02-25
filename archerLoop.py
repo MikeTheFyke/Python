85ed40965a620f21ad8bb2cf14525457e3c95379
MikeTheFyke <mikefyke@hotmail.com>
2020-02-25 14:01:24 -0500

    Added refacturing to archerloop

--- archerLoop.py
+++ archerLoop.py
@@ -36,3 +36,40 @@ extensionNumber = input("\n Please Enter An Agents extension Number   : ")
 print("---------------------------------------\n")
 print(agents[extensionNumber] + " -- " +roles[extensionNumber])
 print("\n---------------------------------------\n")
+
+## Other Methods
+
+# DISPLAY List Using the range method 
+print("\n----------RangeMethod------------------\n")
+for index in range(0, len(agents)):
+    print(agents[index])
+    print(roles[index])
+
+# DISPLAY List Using the enumerate method 
+print("\n----------EnumerateMethod--------------\n")
+for index, agent in enumerate(agents):
+    print(agents)
+    print(roles[index])
+
+# Display Using Refacturing
+
+agentsRoles = [
+    ["Operator","Cheryl Tunt - Your Not My Supervisor"],
+    ["Malory Archer", "Agency Head"],
+    ["Sterling Archer", "Agent In Command - He Said Commandingly"],
+    ["Lawna Kane", "Agent In Command - She Said Commandingly"],
+    ["Cyril Figgis", "Sub Agent - Suppressing Fire"],
+    ["Ray Gillette", "Sub Agent - Half Freaking Robot"],
+    ["Pam Poovey", "Sub Agent - Strong/Big As An Ox"],
+    ["Dr. Krieger", "Scientist - Might Be A Clone Of Adolf Hitler"],
+    ["Barry Dillon", "Target Practice - World Record Ricochette"]
+]
+
+
+print ("\nRefactured List AGENTS/ROLES\n")
+for agentRole in agentsRoles:
+    agent01 = agentRole[0]
+    role01 = agentRole[1]
+    print(agent01 + " - " + role01)
+
+print("\n")
