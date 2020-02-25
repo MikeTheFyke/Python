b29fd4e882fce932e92ea083e3edc5b41c110acc
MikeTheFyke <mikefyke@hotmail.com>
2020-02-25 15:59:27 -0500

    character selection and removal specailString

--- specialString.py
+++ specialString.py
@@ -1,7 +1,19 @@
 userString = raw_input("\nPlease enter a word of sentence for me to count :) \n")
+print("\n")
 
 for index, letter in enumerate(userString):
     print (letter + " is the " + str(index + 1) + " th letter of the String.")
    
 length = str(len(userString))
-print("Your Word of sentence is " + length + " characters long.")
+print("\nYour Word or sentence is " + length + " characters long.")
+
+# Vowel removal/replacment
+unVoweled = str(userString)
+vowels = 'aeiou'
+
+for vowel in vowels:
+    unVoweled = unVoweled.replace(vowel, '')
+
+print ("\nHere is your submission without vowels : " + unVoweled + ".")
+print ("\n")
+#
