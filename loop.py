fec3758ac939eb38993edd5d31ee7397e43a6142
MikeTheFyke <mikefyke@hotmail.com>
2020-02-25 11:45:15 -0500

    Added list data

--- loop.py
+++ loop.py
@@ -1,1 +1,12 @@
+rockosML = [
+    "Rocko Rama",
+    "Spunky",
+    "Heffer Wolfe",
+    "Ed BigHead",
+    "Bev BigHead",
+    "Filburt Turtle",
+    "Earl The Dog",
+    "Paula Hutchison"
+]
+
 for item in collection
