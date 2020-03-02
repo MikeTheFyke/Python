ec5490a1ecbdefeb844f7c3f8545867382cdc068
MikeTheFyke <mikefyke@hotmail.com>
2020-03-02 13:40:44 -0500

    ifelse methods

---
+++ ifelse.py
@@ -0,0 +1,39 @@
+beers = [
+    ["Budweiser","Flavoured Water"],
+    ["BlueLight","Light"],
+    ["Moosehead","Light"],
+    ["Moosehead","Non-Alcoholic"],
+    ["Moosehead","Regular"]
+]
+
+print("\n_______________Welcome To The Beer Emporium_______________\n__________________________________________________________\n")
+
+for listItem in beers:
+    listitemBrand = listItem[0]
+    listitemDraft = listItem[1]
+
+    print("Available Brands : " + str(listitemBrand) + " Available Drafts : " + str(listitemDraft))
+
+userBeer = raw_input("\nWhat brand of beer would you like : ")
+userInput = str(userBeer)
+
+userDraft = raw_input("What draft of beer would you like : ")
+userDraftInput = str(userDraft)
+
+cart = []
+wrongBeerLookedAt = 0
+
+for beer in beers:
+    brandOfBeer = beer[0]
+    draft = beer[1] 
+    if brandOfBeer is userInput and draft is userDraftInput:
+        print("Yes that beer is available")
+        cart.append(beer)
+        break               # break keyword used for ending the for loop once we can confirm a users selection.
+    else:
+        wrongBeerLookedAt += 1
+
+if len(cart) is 0:
+        cart.append("Empty")
+        print("\n Sorry We do not have that beer. I looked through " + str(wrongBeerLookedAt) + " kinds of beer looking for it.\n" )
+
