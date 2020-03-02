c6869c29ee3fa3384cb10a0c6e2219c842238e28
MikeTheFyke <mikefyke@hotmail.com>
2020-03-02 12:11:13 -0500

    Stylized booleans

--- booleans.py
+++ booleans.py
@@ -1,12 +1,17 @@
 # This will equate to true
 theBoys = ['Wayne','Squirrelly Dan','Daryl','Katy']
-bool(theBoys)
+theBoysBoolean = bool(theBoys)
 
 # This will equate to False
 theOtherBoys = []
-bool(theOtherBoys)
+otherBoysBoolean = bool(theOtherBoys)
+
 
 # This will equate to true
 shortSpeech = ("\nYou knew your pal had come into money when he started throwing out \nperfectly good pistachios like he was above cracking em open with a\nbox cutter like the rest of us\n")
 print(shortSpeech)
-bool(shortSpeech)
+speechBoolean = bool(shortSpeech)
+
+print ( "TheBoys Boolean   : " + str(theBoysBoolean) + "\n")
+print ( "OtherBoys Boolean : " + str(otherBoysBoolean) + "\n")
+print ( "ShortSpeech Boolean : " + str(speechBoolean) + "\n")
