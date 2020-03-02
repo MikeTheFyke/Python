31e021c036f55c11f79118b029e50f55eea2f4d6
MikeTheFyke <mikefyke@hotmail.com>
2020-03-02 12:05:20 -0500

    Added notes for string to booleans

--- booleans.py
+++ booleans.py
@@ -6,5 +6,6 @@ bool(theBoys)
 theOtherBoys = []
 bool(theOtherBoys)
 
+# This will equate to true
 shortSpeech = ("\nYou knew your pal had come into money when he started throwing out \nperfectly good pistachios like he was above cracking em open with a\nbox cutter like the rest of us\n")
 print(shortSpeech)
