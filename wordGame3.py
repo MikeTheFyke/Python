246ecf9639d25c28a2245fccd062f30175d29cfc
MikeTheFyke <mikefyke@hotmail.com>
2020-03-02 10:51:48 -0500

    Input initial wordGame3 code

---
+++ wordGame3.py
@@ -0,0 +1,44 @@
+proseString = """
+Hi mom,
+
+Just writing to tell you that I've quit my job as
+a OCCUPATION and I'm moving to COUNTRY.
+The truth is, I've always been passionate
+about PLURAL_NOUN, and COUNTRY is the
+best place in the world to build a career
+around them. I'll need to start a small-- At first,
+all I'll be allowed to do is to VERB near them,
+but when people see how ADJECTIVE I can be,
+I'm sure to rise to the top.
+
+Dont't worry about me, and tell dad to take
+good care of my PERSONAL_ITEM. 
+I'll be sure to call every Holiday.
+
+Regards,
+
+NAME
+
+"""
+
+proseString2 = proseString
+
+ocInput = raw_input(" Please Enter An Occupation   : ")
+proseString2 = proseString2.replace("OCCUPATION", ocInput)
+
+countryInput = raw_input(" Please Enter A Country   : ")
+proseString2 = proseString2.replace("COUNTRY", countryInput)
+
+pluralNounInput = raw_input(" Please Enter A Plural Noun   : ")
+proseString2 = proseString2.replace("PLURAL_NOUN", pluralNounInput)
+
+verbInput = raw_input(" Please Enter An Action   : ")
+proseString2 = proseString2.replace("VERB", verbInput)
+
+adjectiveInput = raw_input(" Please Enter An Adjective   : ")
+proseString2 = proseString2.replace("ADJECTIVE", adjectiveInput)
+
+personalItem = raw_input(" Please Enter A Personal Item    : ")
+proseString2 = proseString2.replace("PERSONAL_ITEM", personalItem)
+
+print(proseString2)
