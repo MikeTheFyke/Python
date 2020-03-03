b3bc53a8bad8a90599722d0e5ed28011b2824e2a
MikeTheFyke <mikefyke@hotmail.com>
2020-03-03 13:54:30 -0500

    Successsul passwordchecker2

--- passwordchecker2.py
+++ passwordchecker2.py
@@ -4,7 +4,7 @@ passwordInput = input("\nPlease enter you Password : ")
 passwordVerInput = input("\nPlease re-enter you Password : ")
 
 
-if passwordInput == passwordVerInput and username == usernameInput:
-    print ("\n Congratualtions account created :) \n")
+if passwordInput == passwordVerInput:
+    print ("\n Congratualtions account created " + usernameInput + " :) \n" )
 else:
     print ("\n Sorry passwords did not match :( )\n")
