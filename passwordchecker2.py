ead4d7e45ac3cafc80d381c3b2d68a0a66fc5732
MikeTheFyke <mikefyke@hotmail.com>
2020-03-03 14:00:19 -0500

    Added 8 character length checker to passwordchecker2

--- passwordchecker2.py
+++ passwordchecker2.py
@@ -1,10 +1,11 @@
 
 usernameInput = raw_input("\nPlease enter your Username : ")
-passwordInput = input("\nPlease enter you Password : ")
-passwordVerInput = input("\nPlease re-enter you Password : ")
+passwordInput = input("\nPlease enter you Password (8 Characters Please) : ")
+passwordVerInput = input("\nPlease re-enter you Password (8 Characters Please) : ")
+passwordLength = len(str(passwordInput))
 
 
-if passwordInput == passwordVerInput:
-    print ("\n Congratualtions" + usernameInput + "account created :) \n" )
+if passwordInput == passwordVerInput and passwordLength >= 8:
+    print ("\n Congratualtions" + usernameInput + "account created :) \n")
 else:
     print ("\n Sorry passwords did not match :( \n")
