0b1e93cccdea50ede3041ced44ac1b16cc83c141
MikeTheFyke <mikefyke@hotmail.com>
2020-03-03 14:53:42 -0500

    Added basic logic to wordGame4

---
+++ wordGame4.py
@@ -0,0 +1,12 @@
+superBasic = [
+    "Title",
+    "Prose String With a REPLACEMENT, and also a SECOND_REPLACEMENT to illustrate the point",
+    [
+        ["Replacement prompt","REPLACEMENT"],
+        ["Another replacement prompt","SECOND_REPLACEMENT"]
+    ]
+]
+
+stories = [
+    superBasic
+]
