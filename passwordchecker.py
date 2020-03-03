40043906c891fd0b445714b558eb38b3d0a4e4a3
MikeTheFyke <mikefyke@hotmail.com>
2020-03-03 13:49:01 -0500

    Finished username and password check

--- passwordchecker.py
+++ passwordchecker.py
@@ -1,10 +1,10 @@
-username ='Joe'
+username = "Joe"
 password = 1234
 
-passwordInput = input("\nPlease enter you Username : ")
+usernameInput = raw_input("\nPlease enter you Username : ")
 passwordInput = input("\nPlease enter you Password : ")
 
-if password == passwordInput:
+if password == passwordInput and username == usernameInput:
     print ("\n That is the correct password :) \n")
 else:
     print ("\n Sorry wrong password :( )\n")
